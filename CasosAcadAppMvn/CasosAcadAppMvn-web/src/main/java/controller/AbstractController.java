9bc65e29752a0adec9d5e803bed3e035d645d2c4
GadMurcia <guillermox001@gmail.com>
2017-04-01 20:22:27 -0600

    modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/CasosAcadAppMvn-ear-1.0-SNAPSHOT/CasosAcadAppMvn-ejb-1.0-SNAPSHOT.jar 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/CasosAcadAppMvn-ear-1.0-SNAPSHOT/CasosAcadAppMvn-web-1.0-SNAPSHOT.war 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/maven-archiver/pom.properties 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/CasosAcadAppMvn-ejb-1.0-SNAPSHOT.jar 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/maven-archiver/pom.properties 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/target/CasosAcadAppMvn-web-1.0-SNAPSHOT.war 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/target/maven-archiver/pom.properties

--- CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java
+++ CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * nuevo agregado para esto del maven y git hook
- * prueba ddos del hook :P
  * prueba 3
  * ni modo
  * :P
