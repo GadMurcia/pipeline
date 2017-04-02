c3cfed98e6fddea5b99536ab85ac4434d325e5f0
GadMurcia <guillermox001@gmail.com>
2017-04-01 18:55:06 -0600

    modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/AbstractFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoDetalleFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoDetalleFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoDetalleRequisitoAtestadoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoDetalleRequisitoAtestadoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoDetalleRequisitoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoDetalleRequisitoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/CasoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/PasoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/PasoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/PasoRequisitoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/PasoRequisitoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/ProcesoDetalleFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/ProcesoDetalleFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/ProcesoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/ProcesoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/RequisitoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/RequisitoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/SolicitudFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/SolicitudFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/TipoPasoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/TipoPasoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/TipoRequisitoFacade.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/classes/casos/acad/acceso/TipoRequisitoFacadeLocal.class 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java
+++ CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * nuevo agregado para esto del maven y git hook
  */
 package controller;
 
