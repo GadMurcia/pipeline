fefbc83a7edcf092fd08596bf7f09cf25f91008b
GadMurcia <guillermox001@gmail.com>
2017-04-01 19:05:58 -0600

    modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java

--- CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java
+++ CasosAcadAppMvn/CasosAcadAppMvn-web/src/main/java/controller/AbstractController.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * nuevo agregado para esto del maven y git hook
+ * prueba ddos del hook :P
+ * prueba 3
  */
 package controller;
 
