165ca1e3c63cb8c3d13b94d7d67cc3fda73cd3bf
GadMurcia <guillermox001@gmail.com>
2017-04-06 08:44:49 -0600

    modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/glassfish/CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/glassfish/Dockerfile 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/glassfish/deployapp.sh 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/mariadb/aria_log.00000001 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/mariadb/aria_log_control 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/mariadb/ib_logfile0 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/mariadb/ibdata1 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/CasosAcadAppMvn-ear-1.0-SNAPSHOT/CasosAcadAppMvn-ejb-1.0-SNAPSHOT.jar 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/CasosAcadAppMvn-ear-1.0-SNAPSHOT/CasosAcadAppMvn-web-1.0-SNAPSHOT.war 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ear/target/maven-archiver/pom.properties 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/CasosAcadAppMvn-ejb-1.0-SNAPSHOT.jar 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-ejb/target/maven-archiver/pom.properties 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/target/CasosAcadAppMvn-web-1.0-SNAPSHOT.war 	modified:   CasosAcadAppMvn/CasosAcadAppMvn-web/target/maven-archiver/pom.properties

--- CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/glassfish/deployapp.sh
+++ CasosAcadAppMvn/CasosAcadAppMvn-ear/composeTPI/glassfish/deployapp.sh
@@ -5,5 +5,4 @@ cd glassfish4/glassfish/bin/
 asadmin start-domain domain1 && \
 echo "AS_ADMIN_PASSWORD=${PASSWORD}" > /tmp/glassfishpwd && \
 asadmin --user=admin --passwordfile=/tmp/glassfishpwd deploy /CasosAcadAppMvn-ear-1.0-SNAPSHOT.ear && \
-asadmin stop-domain domain1&& \
 rm /tmp/glassfishpwd
